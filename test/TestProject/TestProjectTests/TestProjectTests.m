0c14052f3c902efbc32ab77716fcea50ac48047a
pravinkumar1994 <blt-kannan@qweegdvsv456.local>
2016-03-24 11:26:05 +0530

    test

--- test/TestProject/TestProjectTests/TestProjectTests.m
+++ test/TestProject/TestProjectTests/TestProjectTests.m
@@ -5,13 +5,13 @@
 
 @implementation TestProjectTests
 
-- (void)testSuccess
-{
-    STAssertEquals(2 + 2, 4, @"Arithmetic FTW");
-}
-
-- (void)testFail
-{
+//- (void)testSuccess
+//{
+//    STAssertEquals(2 + 2, 4, @"Arithmetic FTW");
+//}
+//
+//- (void)testFail
+//{
     STFail(@"It's easy to write failing tests");
     STFail(@"Some tests have multiple failures");
 }
